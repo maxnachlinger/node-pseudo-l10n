ed0c230f9e1efb4881ab22f7384ca19f7a72bbc3
Max Nachlinger <max.nachlinger@gmail.com>
2014-04-12 23:26:55 -0700

    Cleanup tests

--- tests/endToEndTests.js
+++ tests/endToEndTests.js
@@ -3,14 +3,19 @@ var path = require('path');
 var test = require('tape');
 var sut = require('..');
 var Gettext = require("node-gettext");
-var gt = new Gettext();
 
 test('Parses PO file properly', function (t) {
 	var output = sut({
 		fileContents: fs.readFileSync(path.join(__dirname, '/fixtures/messages.po'))
-	}).toString();
-	var expected = fs.readFileSync(path.join(__dirname, '/fixtures/expected.po')).toString();
-	t.equal(output, expected);
+	});
+
+	var gt = new Gettext();
+	gt.addTextdomain("output", output);
+	gt.addTextdomain("expected", fs.readFileSync(path.join(__dirname, '/fixtures/expected.po')));
+
+	gt.listKeys("expected", "").forEach(function(key) {
+		t.equal(gt.dgettext("output", key), gt.dgettext("expected", key));
+	});
 	t.end();
 });
 
@@ -20,12 +25,11 @@ test('Parses POT file properly', function (t) {
 		potFile: true
 	});
 
+	var gt = new Gettext();
 	gt.addTextdomain("output", output);
 	gt.addTextdomain("expected", fs.readFileSync(path.join(__dirname, '/fixtures/expected.po')));
 
-	var expectedKeys = gt.listKeys("expected", "");
-
-	expectedKeys.forEach(function(key) {
+	gt.listKeys("expected", "").forEach(function(key) {
 		t.equal(gt.dgettext("output", key), gt.dgettext("expected", key));
 	});
 	t.end();
